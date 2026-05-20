9448be09fa697b2244b4198d0d8229a4a5dd2cf2
23130087-AnhHan <23130087@st.hcmuaf.edu.vn>
2026-05-20 21:58:10 +0700

    [COMMON]: Add API response and global exception handling

---
+++ src/main/java/com/orderhub/common/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.orderhub.common.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
